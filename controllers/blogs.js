8bbc13cc8d2badd4f87c064611ae31c3e23456a7
Atoti2 <bogyikormi3@gmail.com>
2024-07-11 08:27:18 +0200

    help

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,14 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
-const User = require("../models/user");
 const jwt = require('jsonwebtoken')
+const userExtractor = require('../utils/middleware').userExtractor
 
-
-
-blogsRouter.get("/", async (request, response) => {
+blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs)
 });
 
-blogsRouter.get('/:id', async (request, response) => {
+blogsRouter.get('/:id', userExtractor, async (request, response) => {
   const blog = await Blog.findById(request.params.id)
   if (blog) {
     response.status(200).json(blog)
@@ -46,7 +44,7 @@ blogsRouter.post("/", async (request, response) => {
   response.status(201).json(savedBlog)
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
+blogsRouter.delete("/:id", userExtractor, async (request, response) => {
 
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
